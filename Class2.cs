f45b29dba5517b34ce61abd0d5d6f692b90b18e9
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-02-20 14:02:22 -0500

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    class Carta
+    {
+        string[] cartas = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        public string carta;
+        public int valor;
+        Random aleatorio = new Random();
+
+        public void Generar()
+        {
+            int indice = aleatorio.Next(0, cartas.Length);
+            carta = cartas[indice];
+
+            if (carta == "A") valor = 1;
+            else if (carta == "J" || carta == "Q" || carta == "K");
+            else valor = int.Parse(carta);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    class Program
+    {
+        static void Main()
+        {
+            Carta carta1 = new Carta();
+
+            for(int i = 0; i < 10; i++)
+            {
+                carta1.Generar();
+                Console.WriteLine(carta1.carta);
+            }
+        }
+    }
+}
