4c82d91b7542822f7b8137351f474382851bbbf9
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-02-20 14:00:35 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figura
+{
+    class Figura
+    {
+        public double b;
+        public double h;
+        public int t;
+        public double a;
+
+        public void Area()
+        {
+            if (t == 0) a = (b * b);
+            else if (t == 2) a = (b * h);
+            else a = ((b * h) / 2);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figura
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Figura figura1 = new Figura();
+            figura1.b = 4;
+            figura1.h = 4;
+            figura1.t = 0;
+
+            figura1.Area();
+            Console.WriteLine("El area es: " + figura1.a);
+
+            Figura figura2 = new Figura();
+            figura2.b = 6;
+            figura2.h = 4;
+            figura2.t = 2;
+
+            figura2.Area();
+            Console.WriteLine("El area es: " + figura2.a);
+
+            Figura figura3 = new Figura();
+            figura3.b = 4;
+            figura3.h = 4;
+            figura3.t = 1;
+
+            figura3.Area();
+            Console.WriteLine("El area es: " + figura3.a);
+
+            if (figura1.a > figura2.a && figura1.a > figura3.a) Console.WriteLine("La primer figura tiene la mayor area");
+            else if (figura2.a > figura1.a && figura2.a > figura3.a) Console.WriteLine("La segunda figura tiene la mayor area");
+            else Console.WriteLine("La tercera figura tiene la mayor area");
+        }
+    }
+}
