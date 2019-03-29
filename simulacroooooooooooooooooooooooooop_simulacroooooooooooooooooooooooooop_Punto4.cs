870e3240a15e8bb80422bdd173a47067d4a06c07
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-03-29 09:58:22 -0500

    Add files via upload

---
+++ simulacroooooooooooooooooooooooooop_simulacroooooooooooooooooooooooooop_Punto4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Punto4
+{
+
+    public delegate void Enconstruccion();
+    public static event Enconstruccion enco;
+    public Punto4()
+    {
+        enco();
+    }
+
+}
+
