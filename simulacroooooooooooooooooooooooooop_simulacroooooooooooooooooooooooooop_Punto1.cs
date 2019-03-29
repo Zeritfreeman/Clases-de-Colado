b3a63a829069b411c68274ec8a92db2dc197703b
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-03-29 09:50:12 -0500

    Add files via upload

---
+++ simulacroooooooooooooooooooooooooop_simulacroooooooooooooooooooooooooop_Punto1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class Punto1
+    {
+    public int x;
+    public static int y;
+    public Punto1()
+    {
+        Punto4.enco += Punto4_enco;
+    }
+
+    public Punto1(int a_, int b_)
+    {
+        Punto4.enco += Punto4_enco;
+    }
+    public Punto1(string txt)
+    {
+        Punto4.enco += Punto4_enco;
+    }
+    private void Punto4_enco()
+    {
+        Console.WriteLine("\n4 ha sido creado");
+    }
+}
+
