cf71dad3bce61d6cf3520afa764fef8417eb1fc3
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-03-29 09:59:22 -0500

    Add files via upload

---
+++ simulacroooooooooooooooooooooooooop_simulacroooooooooooooooooooooooooop_Punto3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Punto3 : Punto2
+{
+    public Punto3(double v1_, double v2_, double v3_, double v4_) : base(v1_, v2_, v3_, v4_)
+    {
+    }
+    public override double Calculopromedio()
+    {
+        double promedio = (v3 + v4)/ 2;
+        return promedio;
+    }
+
+}
